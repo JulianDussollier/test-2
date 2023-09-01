09f9de660e1f9852a7f744138069f84eff88c4bc
Julian <dussollier.julian@gmail.com>
2023-09-01 16:10:27 -0400

    creation class personne et affichage sur index

---
+++ Personne.php
@@ -0,0 +1,17 @@
+<?php
+  class Personne{
+  public $name;
+  public $address;
+  public $zipCode;
+  public $phone;
+  public $email;
+  
+public function getName(){
+    return $this->name;
+
+}
+
+public function setName($name){
+    $this->name = $name;
+  }
+}

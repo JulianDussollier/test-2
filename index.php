a4ba67980ed271d0dab650e9a41f8146fc45beb9
Julian <dussollier.julian@gmail.com>
2023-09-12 10:32:54 -0400

    update index exemples

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php
-require_once "Personne.php";
+require_once "class/Person.php";
+require_once "class/Student.php";
 
-$peter = new Personne("Peter");
+$peter = new Person("Peter");
 
 //$peter->setName("Peter c'est une personne");
 echo $peter->getName();
 
 //echo $peter->address;
 //$peter->address = "ma super adresse";
+
+$ines = new Student("Je suis Ines");
+echo "<br>";
+echo $ines->getName();
+echo "<br>";
+$ines->setStudentId(1);
+echo $ines->getStudentId();
+
+echo "<br>";
+echo "<br>";
+
+
+require_once "class/Shapes.php";
+
+
+$circle = new Circle(2);
+echo "Radius: ". $circle->calcArea();
+
+echo "<br>";
+
+$rectangle = new Rectangle(10, 15);
+echo "Rectangle: ".$rectangle->calcArea();
+

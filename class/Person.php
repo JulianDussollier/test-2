fecd91351fce872589420ba6dbaf23f333523477
Julian <dussollier.julian@gmail.com>
2023-09-12 10:32:10 -0400

    init class person student and teacher

---
+++ class/Person.php
@@ -0,0 +1,28 @@
+<?php
+  class Person{
+  private $name;
+  private $address;
+  public $zipCode;
+  public $phone;
+  public $email;
+  
+ public function __construct($name = null){
+    $this->name = $name;
+  }
+public function getName(){
+    return $this->name;
+
+}
+
+public function setName($name){
+    $this->name = $name;
+  }
+
+public function __destruct(){
+    $this->name = "";
+}
+
+
+}
+
+
---
+++ class/Teacher.php
@@ -0,0 +1,7 @@
+<?php
+class Teacher extends Person{
+  public $teacherId;
+  private $salary;
+
+
+}
